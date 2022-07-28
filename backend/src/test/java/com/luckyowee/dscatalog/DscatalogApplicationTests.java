9c8aac623a06d911915d493fa11a28f6ac541052
Leo Torres <leonardo-torres@hotmail.com>
2022-07-28 13:38:11 -0300

    Backend project created

---
+++ backend/src/test/java/com/luckyowee/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckyowee.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
